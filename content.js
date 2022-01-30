7b26fe6151793a7b9bf4ea7666544bc0e6b5163d
Byrch Baker <62627217+byrchbaker@users.noreply.github.com>
2022-01-30 13:54:19 -0600

    Add files via upload

---
+++ content.js
@@ -0,0 +1,13 @@
+// content.js
+var bodyElement = document.getElementsByTagName("body")[0].innerHTML; 
+
+var headElement = document.getElementsByTagName("head")[0].innerHTML; 
+
+
+if(bodyElement.indexOf("clipboardData.setData")>=0){
+    alert("There is a potential copy and paste spoofing on this website. Be careful!");
+}
+
+if(headElement.indexOf("clipboardData.setData")>=0){
+    alert("There is a potential copy and paste spoofing on this website. Be careful!");
+}
